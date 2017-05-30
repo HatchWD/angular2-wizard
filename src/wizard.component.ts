212356ffa6f7c3e0b074385d0da7f376b629aabf
Samuel Norris <sampnorris@gmail.com>
2017-05-31 09:25:26 +1200

    fixed a problem

--- src/wizard.component.ts
+++ src/wizard.component.ts
@@ -13,7 +13,9 @@ import { WizardStepComponent } from './wizard-step.component';
     <div class="card-footer" [hidden]="isCompleted">
         <button id="js-button-prev" type="button" [ngClass]="{'card-footer__btn':true, 'float-left': true, hidden: !hasPrevStep || !activeStep.showPrev}" (click)="previous()">Previous</button>
         <button id="js-button-next" type="button" [ngClass]="{'card-footer__btn':true, 'float-right': true, hidden: !hasNextStep || !activeStep.showNext}" (click)="next()" [disabled]="!activeStep.isValid" [hidden]="">Next</button>
-        <button id="js-button-done" type="button" class="card-footer__btn float-right" (click)="complete()" [disabled]="!activeStep.isValid" [hidden]="hasNextStep">Compare →</button>
+        <button id="js-button-done" type="button" class="card-footer__btn float-right" (click)="complete()" [disabled]="!activeStep.isValid" [hidden]="hasNextStep || isMultiple">Compare →</button>
+        <button id="js-button-done" type="button" class="card-footer__btn float-right" (click)="complete()" [disabled]="!activeStep.isValid" [hidden]="hasNextStep || !isMultiple">View Vacuum →</button>
+
     </div>
   </div>
   `
